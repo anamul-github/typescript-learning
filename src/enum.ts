5c705280b8e4931447d7df641a50b9d9c7d19556
Anamul Hasan <anamul.jobhunt@gmail.com>
2026-05-05 15:09:28 -0500

    explored enum in TypeScript

---
+++ src/enum.ts
@@ -0,0 +1,17 @@
+//enaum : set of fixed string literal stays in one place
+
+//type UserRoles = 'Admin' | 'Editor' | 'Viewer';
+enum UserRoles {
+    Admin = 'Admin',
+    Editor = 'Editor',
+    Viewer = 'Viewer',
+}
+
+const canEdit = (role: UserRoles) => {
+    if(role === UserRoles.Admin || role === UserRoles.Editor) {
+        return true;
+    } else return false;
+};
+
+const isEditPermissible = canEdit(UserRoles.Admin);
+console.log(isEditPermissible);
